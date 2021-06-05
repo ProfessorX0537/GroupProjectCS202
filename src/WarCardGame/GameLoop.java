9c2e7a9c5ab47a3adedad9f48c3ed9c27dc9ac01
ProfessorX0537 <XMHines01@gmail.com>
2021-06-05 12:08:36 -0700

    Fized mutltiple flip issue and made more Rhobust

--- src/WarCardGame/GameLoop.java
+++ src/WarCardGame/GameLoop.java
@@ -63,65 +63,77 @@ public class GameLoop {
         //IF PLAYER(ONE OR TWO) WINS
         //
 
-        boolean Player1 = false;
-        boolean Player2 = false;
-//        Stack<String> player1deck = new Stack<>();
-//        Stack<String> player2deck = new Stack<>();
+
         Stack<String> player1discard = new Stack<>();
         Stack<String> player2discard = new Stack<>();
 
-        while (!Player1 && !Player2) {
+        while (!gamesLoop(player1deck, player2deck, player1discard, player2discard, CardMap)) {
             //TODO - check if players stacks are empty, if so empty
             //TODO - discard stack into normal stack and call shuffle
-            System.out.println("Enter Flip to play a card for the round");
-            //TODO - checking if player wants to flip or quite requires that player input
-            // flip multiple times need to fix this
-            while (!scnr.next().equalsIgnoreCase("flip") || !scnr.next().equalsIgnoreCase("quite")) {
-                System.out.println("Please enter Flip to play next round or enter Quite to end game.");
-                if (scnr.next().equalsIgnoreCase("quite")) {
-                    System.out.println("Game Over...");
-                    break;
-                } if (scnr.next().equalsIgnoreCase("Flip")) {
-
-                    int flipP1 = CardMap.get(player1deck.peek()); // collects value of top card according to the map and stores it in for player 1
-                    int flipP2 = CardMap.get(player2deck.peek()); // collects value of top card according to the map and stores it in for player 2
-                    String P1card = player1deck.pop(); // takes top card off of player 1's stack and stores it
-                    String P2card = player2deck.pop(); // takes top card off of player 2's stack and stores it
-
-                    if (flipP1 > flipP2) {  // checks if player 1's card is greater
-                        System.out.println("Player 1 wins this round!"); // prints round info
-                        System.out.println(P1card + " is bigger than " + P2card);
-                        System.out.println(P1card + " and " + P2card + " goes to Player 1's discard ");
-                        player1discard.push(P1card); // puts both cards from the round into the winning players
-                        player1discard.push(P2card); // discard stack for later
-                        if (player1deck.size() == 52) { // checks to see if the player has won
-                            System.out.println("Player 1 has won the game, they have acquired all 52 cards");
-                            Player1 = true;
-                        }
-
-                    } else if (flipP1 < flipP2) { // checks if player 2's card is greater
-                        System.out.println("Player 2 wins this round!"); // prints round info
-                        System.out.println(P2card + " is bigger than " + P1card);
-                        System.out.println(P1card + " and " + P2card + " goes to Player 2's discard ");
-                        player2discard.push(P1card); // puts both cards from the round into the winning players
-                        player2discard.push(P2card); // discard stack for later
-                        if (player2deck.size() == 52) { // checks to see if the player has won
-                            System.out.println("Player 2 has won the game, they have acquired all 52 cards");
-                            Player2 = true;
-                        }
-                    } else { // this means that the cards were the same value
-                        //TODO - create tie rules
-                        // probably needs two temp stacks to hold extra cards
-                        // flip 3 cards the 3rd card decides who wins the "war"
-                        // winning player collects all cards flipped. go to discard stack
+            System.out.println("Enter \"Flip\" to play a card for the round");
+            String userInput = scnr.next();
+            if (userInput.equalsIgnoreCase("Flip")) {
+                continue;
+            } else {
+                while (!userInput.equalsIgnoreCase("Flip") || !userInput.equalsIgnoreCase("Quite")) {
+                    System.out.println("Please enter \"Flip\" to play the next round or enter \"Quite\" to end game.");
+                    userInput = scnr.next();
+                    if (userInput.equalsIgnoreCase("Flip") || userInput.equalsIgnoreCase("Quite")) {
+                        break;
                     }
                 }
+            }
+
+            if (userInput.equalsIgnoreCase("Flip")) {
+                gamesLoop(player1deck, player2deck, player1discard, player2discard, CardMap);
+            } else {
+                System.out.println("Game over...");
+                break;
             }
         }
     }
 
     // GAME LOOP MAY NEED TO BE ITS OWN METHOD UNSURE FOR NOW
-    public static void GameLoop(Stack<String> Deck1, Stack<String> Deck2) { //
+    public static boolean gamesLoop(Stack<String> player1deck, Stack<String> player2deck, Stack<String> player1discard,
+                                Stack<String> player2discard, HashMap<String, Integer> CardMap) { //
+        boolean Player1 = false;
+        boolean Player2 = false;
 
+        //if (scnr.next().equalsIgnoreCase("Flip")) {
+
+            int flipP1 = CardMap.get(player1deck.peek()); // collects value of top card according to the map and stores it in for player 1
+            int flipP2 = CardMap.get(player2deck.peek()); // collects value of top card according to the map and stores it in for player 2
+            String P1card = player1deck.pop(); // takes top card off of player 1's stack and stores it
+            String P2card = player2deck.pop(); // takes top card off of player 2's stack and stores it
+
+            if (flipP1 > flipP2) {  // checks if player 1's card is greater
+                System.out.println("Player 1 wins this round!"); // prints round info
+                System.out.println(P1card + " is bigger than " + P2card);
+                System.out.println(P1card + " and " + P2card + " goes to Player 1's discard!");
+                player1discard.push(P1card); // puts both cards from the round into the winning players
+                player1discard.push(P2card); // discard stack for later
+                if (player1deck.size() + player1discard.size() == 52) { // checks to see if the player has won
+                    System.out.println("Player 1 has won the game, they have acquired all 52 cards");
+                     return true; // wins game
+                }
+
+            } else if (flipP1 < flipP2) { // checks if player 2's card is greater
+                System.out.println("Player 2 wins this round!"); // prints round info
+                System.out.println(P2card + " is bigger than " + P1card);
+                System.out.println(P1card + " and " + P2card + " goes to Player 2's discard!");
+                player2discard.push(P1card); // puts both cards from the round into the winning players
+                player2discard.push(P2card); // discard stack for later
+                if (player2deck.size() + player2discard.size() == 52) { // checks to see if the player has won
+                    System.out.println("Player 2 has won the game, they have acquired all 52 cards");
+                    return true; // wins game
+                }
+            } else { // this means that the cards were the same value
+                //TODO - create tie rules
+                // probably needs two temp stacks to hold extra cards
+                // flip 3 cards the 3rd card decides who wins the "war"
+                // winning player collects all cards flipped. go to discard stack
+            }
+       // }
+        return false;
     }
 }
